bdccdf9d6598178904ad879b21f31c10c9392892
efrice <719431749@qq.com>
2024-04-10 19:27:05 +0800

    deploy: docs

--- components.d.ts
+++ components.d.ts
@@ -9,11 +9,9 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Box: typeof import('./src/components/Box.vue')['default']
     Boxes: typeof import('./src/components/Boxes.vue')['default']
     Confetti: typeof import('./src/components/Confetti.vue')['default']
     Game: typeof import('./src/components/Game.vue')['default']
-    Grid: typeof import('./src/components/Grid.vue')['default']
     Map: typeof import('./src/components/Map.vue')['default']
     Message: typeof import('./src/components/Message.vue')['default']
     Mines: typeof import('./src/components/Mines.vue')['default']
@@ -22,7 +20,6 @@ declare module '@vue/runtime-core' {
     Player: typeof import('./src/components/Player.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Select: typeof import('./src/components/Select.vue')['default']
     SelectLevel: typeof import('./src/components/SelectLevel.vue')['default']
     TheFooter: typeof import('./src/components/TheFooter.vue')['default']
     Wall: typeof import('./src/components/Wall.vue')['default']
---
+++ docs/assets/_...all_-6d5f2e61.js
@@ -0,0 +1,1 @@
+import{m as e,c,o}from"./index-35f8c1f4.js";const _={};function n(t,r){return o(),c("div",null," Not Found ")}const s=e(_,[["render",n]]);export{s as default};
